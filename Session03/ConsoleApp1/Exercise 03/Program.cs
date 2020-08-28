0b4adfae44a5f10617839a4c9c07b2121e0b13ee
LAPTOP-JCGE0C4K\Lenovo <nilssonbelinda@outlook.com>
2020-08-28 13:08:21 +0200

    Progress on number app.

---
+++ Session03/ConsoleApp1/Exercise 03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
+            var input = Console.ReadLine();
+            var inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            double[] numberArray = new double[inputArray.Length];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                try
+                {
+                    numberArray[i] = Convert.ToDouble(inputArray[i]);
+                }
+                catch (Exception)
+                {
+                    numberArray[i] = 0;
+                }
+
+                foreach (var number in numberArray)
+                {
+                    Console.WriteLine("Värde: " + number.ToString());
+                }
+            }
+          
+            
+        }
+    }
+}
